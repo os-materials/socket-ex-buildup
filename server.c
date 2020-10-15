9cbff7776e90a1179e39032fa3a3aea4f8fcb41f
Nathan Eloe <powerofazure@gmail.com>
2020-10-15 10:34:06 -0500

    Create sockaddr and socket: step 3

--- server.c
+++ server.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
 char * buffer = NULL;
+int sock;
 
 void read_stdin();
 
 int main()
 {
+	struct sockaddr_in my_address;
+	my_address.sin_family = AF_INET;
+	my_address.sin_addr.s_addr = INADDR_ANY;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sock == -1)
+	{
+		perror("Error creating socket:");
+		exit(1);
+	}
+
 	return 0;
 
 }
